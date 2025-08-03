90ad6e438491d3854fedbb42dd28748c4fe82de7
Thilina Bowatta <bowattatab@gmail.com>
2025-08-03 13:55:11 +0530

    Now API keys working and Authorized person can use my API

--- CozyComfortAPI/Program.cs
+++ CozyComfortAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using CozyComfortAPI.Data;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var conn = "Data Source=HARSHAMAX_PC;Initial Catalog=CozyComfort;Integrated Security=True;TrustServerCertificate=True";
 builder.Services.AddDbContext<AppDBContext>(op=>op.UseSqlServer(conn));
@@ -16,6 +18,32 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CozyComfortAPI", Version = "v1" });
+
+    // Define the API Key security scheme
+    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "API Key authorization using X-API-KEY header (Example: 'X-API-KEY YOUR_API_KEY')",
+        Name = "X-API-KEY", 
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey 
+    });
+
+   
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "ApiKey" }
+            },
+            new string[] { }
+        }
+    });
+});
 
 var app = builder.Build();
 
